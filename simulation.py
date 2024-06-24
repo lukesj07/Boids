12d622a83a26bfd69615676491c1488bfa833cd6
Luke Jennings <luke@localhost.localdomain>
2024-06-24 16:45:44 -0400

    added screen looping and for loop for generating boids

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@
 import pygame
 import sys
 import math
+import random
 
 pygame.init()
 display = pygame.display
@@ -34,19 +35,24 @@ class Boid:
         svec = [0, 0]
         avg_angle = 0
         for boid in boids:
-            if self != boid and vector_magnitude(self.calculate_distance_vector(boid)) < 500:
+            if self != boid and vector_magnitude(self.calculate_distance_vector(boid)) < 250:
                 num_close_boids += 1
 
                 avg_x += boid.pos[0]
                 avg_y += boid.pos[1]
                 
                 avg_angle += boid.angle
-        
-        com = [avg_x/num_close_boids-self._pos[0], avg_y/num_close_boids-self._pos[1]] #absolute pos?
-        svec = [-(avg_x-self.pos[0]*num_close_boids*0.1), -(avg_y-self.pos[1]*num_close_boids*0.1)] #relative 'goal' velocity vector?
+       
+
+        if num_close_boids == 0:
+            return [450-self.pos[0], 450-self.pos[1]]
+
+        com = [(avg_x/num_close_boids-self._pos[0]*5), (avg_y/num_close_boids-self._pos[1])*5]
+        svec = [-(avg_x-self.pos[0]*num_close_boids*0.01), -(avg_y-self.pos[1]*num_close_boids*0.01)] #relative 'goal' velocity vector?
 
         avg_angle /= num_close_boids # 'goal' angle
-        avec = [ANGLE_FACTOR*math.cos(avg_angle), ANGLE_FACTOR*math.sin(avg_angle)]
+        avec = [ANGLE_FACTOR*math.cos(avg_angle)*200, ANGLE_FACTOR*math.sin(avg_angle)*200]
+
 
         return [com[i]+svec[i]+avec[i] for i in range(len(com))]
 
@@ -97,18 +103,35 @@ def update(boids: list[Boid]) -> None:
 
         tvec = boid.goal_velocity_vector(boids)
         #if i == 0:
-            #print(tvec)
-        if boid.pos[0] < 200 and theta > math.pi/2 and theta < 3*math.pi/2:
+            #print(tvec)]
+        """
+        temp_tvec = tvec
+        if boid.pos[0] < 200: #and theta > math.pi/2 and theta < 3*math.pi/2:
             if i == 0:
-                print("ran")   
-            tvec[0] += 700
+                print("ran")
+            temp_tvec[0] = tvec[0] + 1000
         if boid.pos[0] > 700:
-           tvec[0] -= 700
+            temp_tvec[0] = tvec[0] - 1000
         if boid.pos[1] > 700:
-            tvec[1] += 700
+            temp_tvec[1] = tvec[1] + 1000
         if boid.pos[1] < 200:
-            tvec[1] -= 700
+            temp_tvec[1] = tvec[1] - 1000
+        """
+        flag = False
+        if boid.pos[1] <= 0:
+            print("ran")
+            boid.pos = [boid.pos[0], 900]
+            print(boid.pos)
+            flag = True
+        elif boid.pos[1] >= 900:
+            boid.pos[1] = 0
+        if boid.pos[0] <= 0:
+            boid.pos[0] = 900
+        elif boid.pos[0] >= 900:
+            boid.pos[0] = 0
 
+
+        
         diff_angle = math.acos(dproduct([vx, vy], tvec)/(vector_magnitude([vx, vy])*vector_magnitude(tvec)))
     
         if tvec[1] > vy:
@@ -123,10 +146,17 @@ def update(boids: list[Boid]) -> None:
 def main():
     run = True
     clock = pygame.time.Clock()
-    b1 = Boid([400, 470], 20, math.pi)
-    b2 = Boid([500, 500], 5, 0)
-    b3 = Boid([400, 500], 50, math.pi-2)
-    boids = [b1, b2, b3]
+    boids = []
+    for i in range(10):
+        angle = random.uniform(-1, 1) + math.pi
+        pos = [random.randint(0, 900), random.randint(0,900)]
+        b = Boid(pos, 20, angle)
+        boids.append(b)
+        
+    #b1 = Boid([400, 470], 20, math.pi)
+    #b2 = Boid([500, 500], 5, 0)
+    #b3 = Boid([400, 500], 50, math.pi-2)
+    #boids = [b1, b2, b3]
     while run:
         clock.tick(60)
         keys = pygame.key.get_pressed()
