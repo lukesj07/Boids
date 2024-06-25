6d22e852d83eb86101e5f1a039876f4b5702ee8c
CalebBunch <rcbcg123@gmail.com>
2024-06-24 21:05:41 -0400

    lots of constants

--- simulation.py
+++ simulation.py
@@ -3,15 +3,24 @@ import sys
 import math
 import random
 
-pygame.init()
-display = pygame.display
-surface = display.set_mode((900, 900))
-display.set_caption("Boids")
-
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DT = 1/60
 ANGLE_FACTOR = 1
+BOID_LEG = 8
+BOID_HEIGHT = 18
+SVEC_FACTOR = 0.5
+AVEC_FACTOR = 200
+MVEC_FACTOR = 5
+VISION_RADIUS = 200
+VELOCITY_FACTOR = 150
+
+pygame.init()
+display = pygame.display
+surface = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+display.set_caption("Boids")
 
 class Boid:
     def __init__(self, pos: list[float], vel: float, angle: float) -> None:
@@ -20,15 +29,15 @@ class Boid:
         self._angle = angle
     
     def draw_boid(self):
-        x1 = (-8)*math.cos(self._angle)-(18)*math.sin(self._angle)+self._pos[0]
-        y1 = (-8)*math.sin(self._angle)+(18)*math.cos(self._angle)+self._pos[1]
-        x2 = (8)*math.cos(self._angle)-(18)*math.sin(self._angle)+self._pos[0]
-        y2 = (8)*math.sin(self._angle)+(18)*math.cos(self._angle)+self._pos[1]
+        x1 = (-BOID_LEG)*math.cos(self._angle)-(BOID_HEIGHT)*math.sin(self._angle)+self._pos[0]
+        y1 = (-BOID_LEG)*math.sin(self._angle)+(BOID_HEIGHT)*math.cos(self._angle)+self._pos[1]
+        x2 = (BOID_LEG)*math.cos(self._angle)-(BOID_HEIGHT)*math.sin(self._angle)+self._pos[0]
+        y2 = (BOID_LEG)*math.sin(self._angle)+(BOID_HEIGHT)*math.cos(self._angle)+self._pos[1]
         pygame.draw.polygon(surface, WHITE, [self._pos, (x1, y1), (x2, y2)])
 
     def loop_distance(self, boid: "Boid") -> float:
-        horizontal_dist = 900 - max(self.pos[0], boid.pos[0]) + min(self.pos[0], boid.pos[0])
-        vertical_dist = 900 - max(self.pos[1], boid.pos[1]) + min(self.pos[1], boid.pos[1])
+        horizontal_dist = SCREEN_WIDTH - max(self.pos[0], boid.pos[0]) + min(self.pos[0], boid.pos[0])
+        vertical_dist = SCREEN_HEIGHT - max(self.pos[1], boid.pos[1]) + min(self.pos[1], boid.pos[1])
         return [horizontal_dist, vertical_dist]
 
     def goal_velocity_vector(self, boids: list["Boid"]) -> list[float]:
@@ -38,22 +47,21 @@ class Boid:
         avg_angle = 0
         for boid in boids:
             loop_d = self.loop_distance(boid)
-            if self != boid and (vector_magnitude(self.calculate_distance_vector(boid)) < 200 or loop_d[0] < 200 or loop_d[1] < 200):
+            if self != boid and (vector_magnitude(self.calculate_distance_vector(boid)) < VISION_RADIUS or loop_d[0] < VISION_RADIUS or loop_d[1] < VISION_RADIUS):
                 num_close_boids += 1
                 avg_x += boid.pos[0]
                 avg_y += boid.pos[1]
                 avg_angle += boid.angle
        
         if num_close_boids == 0:
-            return [450-self.pos[0], 450-self.pos[1]]
+            return [SCREEN_WIDTH/2-self.pos[0], SCREEN_HEIGHT/2-self.pos[1]]
 
-        com = [(avg_x/num_close_boids-self._pos[0]*5), (avg_y/num_close_boids-self._pos[1])*5]
-        svec = [-(avg_x-self.pos[0]*num_close_boids*0.5), -(avg_y-self.pos[1]*num_close_boids*0.5)]
-        avg_angle /= num_close_boids # 'goal' angle
-        avec = [ANGLE_FACTOR*math.cos(avg_angle)*200, ANGLE_FACTOR*math.sin(avg_angle)*200]
+        m_vec = [(avg_x/num_close_boids-self._pos[0]*MVEC_FACTOR), (avg_y/num_close_boids-self._pos[1])*MVEC_FACTOR]
+        s_vec = [-(avg_x-self.pos[0]*num_close_boids*SVEC_FACTOR), -(avg_y-self.pos[1]*num_close_boids*SVEC_FACTOR)]
+        avg_angle /= num_close_boids
+        a_vec = [ANGLE_FACTOR*math.cos(avg_angle)*AVEC_FACTOR, ANGLE_FACTOR*math.sin(avg_angle)*AVEC_FACTOR]
 
-        return [com[i]+svec[i]+avec[i] for i in range(len(com))]
-
+        return [m_vec[i]+s_vec[i]+a_vec[i] for i in range(len(m_vec))]
 
     def calculate_distance_vector(self, other: "Boid") -> list[float]:
         return [self._pos[i]-other.pos[i] for i in range(len(self._pos))]
@@ -82,7 +90,6 @@ class Boid:
     def angle(self, newangle: float) -> None:
         self._angle = newangle
 
-
 def dproduct(vec1: list[float], vec2: list[float]) -> list[float]:
     return sum([vec1[i]*vec2[i] for i in range(len(vec1))])
 
@@ -100,19 +107,19 @@ def update(boids: list[Boid]) -> None:
         tvec = boid.goal_velocity_vector(boids)
         
         if boid.pos[1] <= 0:
-            boid.pos[1] = 900
-        elif boid.pos[1] >= 900:
+            boid.pos[1] = SCREEN_HEIGHT
+        elif boid.pos[1] >= SCREEN_HEIGHT:
             boid.pos[1] = 0
         if boid.pos[0] <= 0:
-            boid.pos[0] = 900
-        elif boid.pos[0] >= 900:
+            boid.pos[0] = SCREEN_WIDTH
+        elif boid.pos[0] >= SCREEN_WIDTH:
             boid.pos[0] = 0
 
         diff_angle = math.acos(dproduct([vx, vy], tvec)/(vector_magnitude([vx, vy])*vector_magnitude(tvec)))
         if tvec[1] > vy:
             diff_angle *= -1
         
-        boid.vel = abs(math.cos(diff_angle))*150
+        boid.vel = abs(math.cos(diff_angle))*VELOCITY_FACTOR
         boid.angle += diff_angle*DT
         boid.pos[0] += vx*DT
         boid.pos[1] += vy*DT
@@ -123,7 +130,7 @@ def main():
     boids = []
     for i in range(60):
         angle = random.uniform(-1, 1) + math.pi
-        pos = [random.randint(0, 900), random.randint(0,900)]
+        pos = [random.randint(0, SCREEN_WIDTH), random.randint(0,SCREEN_HEIGHT)]
         b = Boid(pos, random.randint(1, 50), angle)
         boids.append(b)
         
